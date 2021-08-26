9911b5d0b5927160b07cd4b61fec1675563d8150
Clientastisch <51415855+Clientastisch@users.noreply.github.com>
2021-08-26 03:23:51 +0200

    feat(repositories): usage of optional

--- src/main/java/me/micartey/nura/repositories/UserRepository.java
+++ src/main/java/me/micartey/nura/repositories/UserRepository.java
@@ -3,11 +3,15 @@ package me.micartey.nura.repositories;
 import me.micartey.nura.entities.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<UserEntity, String> {
 
     UserEntity findByMail(String mail);
 
     boolean existsByMail(String mail);
+
+    Optional<UserEntity> findByMailAndPassword(String mail, String password);
 
     boolean existsByMailAndPassword(String mail, String password);
 
