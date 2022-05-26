7ba63a7cab23b7c0be08ef6d86e086e0c7e29027
micartey <me@micartey.dev>
2022-05-27 00:24:25 +0200

    fix: resetbody

--- src/main/java/me/micartey/nura/bodies/ResetBody.java
+++ src/main/java/me/micartey/nura/bodies/ResetBody.java
@@ -1,10 +1,8 @@
 package me.micartey.nura.bodies;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class ResetBody {
     private final String mail;
 }
